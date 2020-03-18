53170112e835db33fa47e250c2462806d34f0f5d
Bola Ajose <bolaa.ajose@gmail.com>
2020-03-18 10:09:52 +0000

    completed step 3, attempting to complete step 4

--- string_calculator.js
+++ string_calculator.js
@@ -2,7 +2,26 @@ const stringCalculator = function(numberString) {
     if (numberString === "") {
         return 0;
     };
-    const numbers = numberString.split(",");
+    //"//;\n1;2"
+    if (numberString.startsWith("//")) {
+        // let seperator = numberString.substring(2, 3);
+        // numberString = numberString.substring(3);
+        var separator = ";";
+        var reg = new RegExp(separator, "g");
+        numberString.replace(reg, ",");
+        console.log(numberString);
+    }
+
+    // var separator = ";";
+    // var regex = new RegExp(separator, "g");
+
+    // var input = "1;2";
+    // var output = input.replace(regex, ",");
+    // alert(output); 
+
+    const numbersBefore = numberString.replace(/\n/g, ",");
+    const numbers = numbersBefore.split(",");
+
     if (numbers.length === 1) {
         return Number(numbers[0]);
     } else {
--- string_calculator_test.js
+++ string_calculator_test.js
@@ -18,4 +18,16 @@ describe('stringCalculatorMust', () => {
         const result = stringCalculator("1,2,3,4,5,6,7,8,9");
         assert.strictEqual(result, 45);
     });
+    it('recognise newlines as well as commas as separators and still return its sum as an interger', () => {
+        const result = stringCalculator("1\n2,3");
+        assert.strictEqual(result, 6);
+    });
+    it('supports custom separators', () => {
+        const result = stringCalculator("//;\n1;2");
+        assert.strictEqual(result, 3);
+    });
+    // it('throw an exception when negatives are passed detailing negatives not allowed, and the negative that was passed', () => {
+    //     const result = stringCalculator("1,-2,-3");
+    //     assert.strictEqual(result, 'error: negatives not allowed: -2 -3');
+    // });
 });
